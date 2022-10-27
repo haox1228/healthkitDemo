41f8e3742fdae4ae05a24468cd08bb84e1fb9587
haox1228 <48774325+haox1228@users.noreply.github.com>
2022-10-27 14:42:36 -0400

    Initial Commit

---
+++ HealthKitDemo/HealthKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitDemoApp.swift
+//  HealthKitDemo
+//
+//  Created by 马浩轩 on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
